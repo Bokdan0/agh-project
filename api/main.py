f97758e15d326a567135ae10d0e86a53d6cabaf0
Bokdan0 <bokdan@student.agh.edu.pl>
2023-06-27 18:58:55 +0200

    to zyje!

--- api/main.py
+++ api/main.py
@@ -1,13 +1,59 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 from api import customers
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class StudentCreateSchema(BaseModel):
+    first_name: str
+    last_name: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "first_name": "Jerzetta",
+                "last_name": "Kłosińska",
+            }
+        }
+
+class StudentUpdateSchema(BaseModel):
+    first_name: str
+    last_name: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "first_name": "Grzegorz",
+                "last_name": "Brzęczyszczykiewicz",
+            }
+        }
+
+
+class Student(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+
+STUDENTS = {}
+
+@app.put("/student/{student_id}")
+async def update_student(student_id: int, student:StudentUpdateSchema):
+    STUDENTS[id] = Student(**Student.dict(), id = student_id)
+    return {"update_student"}
+
+@app.get("/students")
+async def get_students():
+    return STUDENTS
+
+@app.post("/students")
+async def create_student(student: StudentCreateSchema):
+    id = len(STUDENTS) + 1
+    new_student = Student(**student.dict(), id=id)
+    STUDENTS[id] = new_student
+    return new_student
+
 
 origins = ["*"]
 
